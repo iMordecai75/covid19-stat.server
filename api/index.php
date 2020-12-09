8765677af31648bc76d91dc08fd1ba8230d61aa9
federico masci <federico.masci@coni.it>
2020-12-09 09:06:11 +0100

    - Aggiunta classe ApiResponse per uniformare le risposte dell'API - Implementata per i dati nazionali

--- api/index.php
+++ api/index.php
@@ -6,8 +6,10 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT,DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 header("Content-Type: application/json; charset=UTF-8");
+require_once 'classes/ApiResponse.php';
 require_once 'utilities/connection.php';
 
+$response = new ApiResponse();
 $endpoint = '/dpc-covid19-ita-andamento-nazionale.json';
 
 if (isset($_GET['task']) && $_GET['task'] == 'latest') {
@@ -16,11 +18,13 @@ if (isset($_GET['task']) && $_GET['task'] == 'latest') {
 try {
     $result = Connection::cURLdownload($endpoint);
 
-    echo $result;
+    $response->status = 'OK';
+    $response->items = json_decode($result);
+
+    echo $response->toJson();
 } catch (\Throwable $th) {
-    $result = array(
-        'status' => 'KO',
-        'error' => $th->getMessage()
-    );
-    echo json_encode($result);
+    $response->status = 'KO';
+    $response->msg = $th->getMessage();
+
+    echo $response->toJson();
 }
