43b0b72cda643167bde1b5000a23b81eb092ca85
federico masci <federico.masci@coni.it>
2020-12-10 10:08:06 +0100

    - Aggiunto metodo che restituisce una anotazione

--- api/annotations.php
+++ api/annotations.php
@@ -76,48 +76,66 @@ switch($method) {
     case 'DELETE':
     break;
     case 'GET':
-        try {
-            $query = "SELECT Annotation_iId, Annotation_sValue, Annotation_sContent FROM tblAnnotations";
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            try {
+                $query = "SELECT Annotation_iId, Annotation_sValue, Annotation_sContent FROM tblAnnotations WHERE Annotation_iId = ?";
+                $stmt = $dbh->prepare($query);
+                $stmt->execute([$_GET['id']]);
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-            $stmt = $dbh->prepare($query);
-            $stmt->execute();
-            $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                $response->status = 'OK';
+                $response->item = $row;
 
-        } catch (PDOException $th) {
-            $response->status = 'KO';
-            $response->msg = $th->getMessage();
+                echo $response->toJson();
+            } catch (PDOException $th) {
+                $response->status = 'KO';
+                $response->msg = $th->getMessage();
 
-            echo $response->toJson();
-            exit;
-        }
-        if (isset($_GET['task']) && $_GET['task'] == 'backend') {
-            $response->status = 'OK';
-            $response->items = $rows;
-
-            echo $response->toJson();
+                echo $response->toJson();
+                exit;
+            }
         } else {
-            $result = array();
-            foreach($rows as $row) {
-                $tmp = new stdClass();
-                $tmp->type = "line";
-                $tmp->mode = "vertical";
-                $tmp->scaleID = "x-axis-0";
-                $tmp->value = $row['Annotation_sValue'];
-                $tmp->borderColor = "black";
-                $tmp->borderWidth = 2;
-                $tmp->label = new stdClass();
-                $tmp->label->enabled = true;
-                $tmp->label->fontColor = "orange";
-                $tmp->label->content = $row['Annotation_sContent'];
+            try {
+                $query = "SELECT Annotation_iId, Annotation_sValue, Annotation_sContent FROM tblAnnotations";
+                $stmt = $dbh->prepare($query);
+                $stmt->execute();
+                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } catch (PDOException $th) {
+                $response->status = 'KO';
+                $response->msg = $th->getMessage();
 
-                $result[] = $tmp;
+                echo $response->toJson();
+                exit;
+            }
+            if (isset($_GET['task']) && $_GET['task'] == 'backend') {
+                $response->status = 'OK';
+                $response->items = $rows;
+
+                echo $response->toJson();
+            } else {
+                $result = array();
+                foreach ($rows as $row) {
+                    $tmp = new stdClass();
+                    $tmp->type = "line";
+                    $tmp->mode = "vertical";
+                    $tmp->scaleID = "x-axis-0";
+                    $tmp->value = $row['Annotation_sValue'];
+                    $tmp->borderColor = "black";
+                    $tmp->borderWidth = 2;
+                    $tmp->label = new stdClass();
+                    $tmp->label->enabled = true;
+                    $tmp->label->fontColor = "orange";
+                    $tmp->label->content = $row['Annotation_sContent'];
+
+                    $result[] = $tmp;
+                }
+
+                $response->status = 'OK';
+                $response->items = $result;
+
+                echo $response->toJson();
             }
 
-            $response->status = 'OK';
-            $response->items = $result;
-
-            echo $response->toJson();
         }
-
     break;
 }
